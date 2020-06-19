bbc82daf7bd54907588702c5c1cdf9632929e98b
BiancaFSilva <biancaflorianodasilva@gmail.com>
2020-06-19 11:39:03 -0300

    Update restrita.php

--- Cliente - exemploCRUD/restrita.php
+++ Cliente - exemploCRUD/restrita.php
@@ -1,4 +1,20 @@
 <?php
+    session_start();
+
+    if (
+        (!isset($_SESSION['id'])==true) &&
+        (!isset($_SESSION['id'])==true) &&
+        (!isset($_SESSION['id'])==true)) {
+            //Session_destroy();
+            unset($_SESSION['id']);
+            unset($_SESSION['nome']);
+            unset($_SESSION['email']);
+
+            header('Location: index.html');
+        }
+
     echo "Página restrita <br>";
     echo "(Mas nemtão restrita assim)";
 ?>
+
+<a href="sair.php"> Sair </a>
