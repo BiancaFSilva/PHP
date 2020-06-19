1c336b539e3d05739d0e2d478d8dd3c16d18ee37
BiancaFSilva <biancaflorianodasilva@gmail.com>
2020-06-19 11:38:52 -0300

    Update processa.php

--- Cliente - exemploCRUD/processa.php
+++ Cliente - exemploCRUD/processa.php
@@ -1,4 +1,7 @@
 <?php
+    //SESSION -> pseudovariável
+    session_start();
+
     include 'conecta.php';
 
     $email = $_POST['username'];
@@ -8,9 +11,14 @@
 
     $resultado = $conexao->query($consulta);
     $registros = $resultado->num_rows;                  //quantidade de registros
+    
     $resultado_user = mysqli_fetch_assoc($resultado);   //trabalhar a sessão do php
 
     if ($registros == 1) {
+        $_SESSION['id'] = $resultado_user['id'];
+        $_SESSION['nome'] = $resultado_user['nome'];
+        $_SESSION['email'] = $resultado_user['email'];
+
         header('Location: restrita.php');
     } else {
         header('Location: index.html');
